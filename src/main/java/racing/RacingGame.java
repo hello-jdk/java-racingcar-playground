89812d03a2445b301ef2f375a2b4bee3a3194445
정동걸 <ehdrjf2062@gmail.com>
2022-12-01 20:42:59 +0900

    feat(RacingGame): 시도할 횟수만큼 이동 실행

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -8,7 +8,7 @@ public class RacingGame {
     private final List<Car> cars;
     private final RandomGenerator randomGenerator;
 
-    public RacingGame(String[] names,RandomGenerator randomGenerator) {
+    public RacingGame(String[] names, RandomGenerator randomGenerator) {
         Validator.checkRacingGameNameSize(names);
 
         this.randomGenerator = randomGenerator;
@@ -24,5 +24,25 @@ public class RacingGame {
 
     public void tryMove(int tryCount) {
         Validator.checkTryMoveCount(tryCount);
+        for (int stage = 0; stage < tryCount; stage++) {
+            moveCars();
+        }
+    }
+
+    private void moveCars() {
+        for (int carNumber = 0; carNumber < cars.size(); carNumber++) {
+            moveCar(carNumber);
+        }
+    }
+
+    private void moveCar(int carNumber) {
+        if (isCarGoodToGo()) {
+            cars.get(carNumber).move();
+        }
+    }
+
+    private boolean isCarGoodToGo() {
+        int randomNumber = randomGenerator.generate();
+        return randomNumber >= 4;
     }
 }
